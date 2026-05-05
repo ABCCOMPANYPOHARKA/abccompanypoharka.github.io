ee899b9a6ada525dc910d93e384ba45f901a3d21
ABC COMPANY POHARKA <ajaxsihag@gmail.com>
2026-05-05 18:07:13 +0530

    Update Bear.tsx

--- src/components/apps/Bear.tsx
+++ src/components/apps/Bear.tsx
@@ -93,9 +93,8 @@ const Sidebar = ({ cur, setMidBar }: SidebarProps) => {
         {bear.map((item, index) => (
           <li
             key={`bear-sidebar-${item.id}`}
-            className={`pl-6 h-8 hstack cursor-default ${
-              cur === index ? "bg-red-500" : "bg-transparent"
-            } ${cur === index ? "" : "hover:bg-gray-600"}`}
+            className={`pl-6 h-8 hstack cursor-default ${cur === index ? "bg-red-500" : "bg-transparent"
+              } ${cur === index ? "" : "hover:bg-white-600"}`}
             onClick={() => setMidBar(item.md, index)}
           >
             <span className={item.icon} />
@@ -113,18 +112,17 @@ const Middlebar = ({ items, cur, setContent }: MiddlebarProps) => {
       {items.map((item: BearMdData, index: number) => (
         <li
           key={`bear-midbar-${item.id}`}
-          className={`h-24 flex flex-col cursor-default border-l-2 ${
-            cur === index
-              ? "border-red-500 bg-white dark:bg-gray-900"
+          className={`h-24 flex flex-col cursor-default border-l-2 ${cur === index
+              ? "border-red-500 bg-white dark:bg-white-900"
               : "border-transparent bg-transparent"
-          } hover:(bg-white dark:bg-gray-900)`}
+            } hover:(bg-white dark:bg-white-900)`}
           onClick={() => setContent(item.id, item.file, index)}
         >
           <div className="h-8 mt-3 hstack">
             <div className="-mt-1 w-10 vstack text-c-500">
               <span className={item.icon} />
             </div>
-            <span className="relative flex-1 font-bold" text="gray-900 dark:gray-100">
+            <span className="relative flex-1 font-bold" text="white-900 dark:white-100">
               {item.title}
               {item.link && (
                 <a
@@ -242,17 +240,17 @@ const Bear = () => {
 
   return (
     <div className="bear font-avenir flex h-full">
-      <div className="w-44 overflow-auto bg-gray-700">
+      <div className="w-44 overflow-auto bg-white-700">
         <Sidebar cur={state.curSidebar} setMidBar={setMidBar} />
       </div>
-      <div className="w-60 overflow-auto" bg="gray-50 dark:gray-800" border="r c-300">
+      <div className="w-60 overflow-auto" bg="white-50 dark:white-800" border="r c-300">
         <Middlebar
           items={state.midbarList}
           cur={state.curMidbar}
           setContent={setContent}
         />
       </div>
-      <div className="flex-1 overflow-auto" bg="gray-50 dark:gray-800">
+      <div className="flex-1 overflow-auto" bg="white-50 dark:white-800">
         <Content contentID={state.contentID} contentURL={state.contentURL} />
       </div>
     </div>
