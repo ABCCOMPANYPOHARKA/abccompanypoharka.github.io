9bf7d6dbc6fc33d9db466c535fded71bb6d32eb9
ABC COMPANY POHARKA <ajaxsihag@gmail.com>
2026-05-05 16:05:22 +0530

    Update VSCode.tsx

--- src/components/apps/VSCode.tsx
+++ src/components/apps/VSCode.tsx
@@ -2,7 +2,7 @@ export default function VSCode() {
   return (
     <iframe
       className="size-full bg-[#202020]"
-      src="https://github.com/ABCCOMPANYPOHARKA/abccompanypoharka.github.io"
+      src="https://github.com/ABCCOMPANYPOHARKA/abccompanypoharka.github.io/blob/main/README.md"
       title="VSCode"
     />
   );
