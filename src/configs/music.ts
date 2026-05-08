f3372ed17aaa75ba879d38cfb92c9d8cc812fd77
ABC COMPANY POHARKA <ajaxsihag@gmail.com>
2026-05-08 17:28:06 +0530

    Update music.ts

--- src/configs/music.ts
+++ src/configs/music.ts
@@ -1,7 +1,7 @@
 import type { MusicData } from "~/types";
 
 const music: MusicData = {
-  title: "Sunflower",
+  title: "50 Tola",
   artist: "50 Tola | Vaastav | SunjayDutt",
   cover: "//resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p68252_p_v8_ad.jpg",
   audio: "music/50tola.mp3"
