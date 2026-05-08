ec63459383e0dc8ef7613b1b61b17800137ac07e
ABC COMPANY POHARKA <ajaxsihag@gmail.com>
2026-05-08 17:22:36 +0530

    ok

--- src/configs/music.ts
+++ src/configs/music.ts
@@ -2,9 +2,9 @@ import type { MusicData } from "~/types";
 
 const music: MusicData = {
   title: "Sunflower",
-  artist: "Post Malone / Swae Lee",
-  cover: "//p1.music.126.net/z0IO1vEsowL9mD_5yzUjeA==/109951163936068098.jpg",
-  audio: "music/sunflower.mp3"
+  artist: "50 Tola | Vaastav | SunjayDutt",
+  cover: "//resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p68252_p_v8_ad.jpg",
+  audio: "music/50tola.mp3"
 };
 
 export default music;
